38f8f934f4987363989770d4249103d95622564a
ahmedadel <ahmedadelmohmaed2@gmail.com>
2021-09-03 23:07:06 +0200

    yamlFile Continue

---
+++ config/eloquent_model_generator.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'namespace'       => 'App\Models',
+    'base_class_name' => \Illuminate\Database\Eloquent\Model::class,
+    'output_path'     => '/App/Models',
+    'no_timestamps'   => false,
+    'date_format'     => null,
+    'connection'      => null,
+    'backup'          => true,
+];
