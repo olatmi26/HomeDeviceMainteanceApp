f02bfaa66cf1962cdfd1796b0427f09725a9cdaa
ahmedadel <ahmedadelmohmaed2@gmail.com>
2021-09-04 20:21:57 +0200

    Application Continue in Developmement First Major Commit

--- routes/auth.php
+++ routes/auth.php
@@ -24,6 +24,13 @@ Route::get('/login', [AuthenticatedSessionController::class, 'create'])
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
                 ->middleware('guest');
 
+Route::prefix('admin')->name('admin.')->group(function () {
+    //admin Authentication Routes
+    Route::prefix('account')->group(function () {
+        Route::get('login', );
+    });    
+});
+
 Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->middleware('guest')
                 ->name('password.request');
